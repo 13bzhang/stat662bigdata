f4aed60d144d4c71ea0df7159040d3b1028885e9
Baobao Zhang <13bzhang@gmail.com>
2014-09-05 23:58:21 -0400

    Baobao finalized R code for big data generation

--- stat662_gen_bigdata.R
+++ stat662_gen_bigdata.R
@@ -1,21 +1,32 @@
 ###########################################
 # STAT 662 Generate Big Data
 
-nr=1000 # number of rows
-# function to sample and create the numerical columns (1-97)
-cnum <- function(nr) {
-  sample(x=c(0.1, 0.2, 0.3),size=nr,replace=T)
+install.packages("rbenchmark")
+library(rbenchmark)
+
+genbig <- function(times,size,filename){
+  file.create(filename)
+  gensize <- function(size,filename){
+    nr <- size # number of rows
+    # function to sample and create the numerical columns (1-97)
+    cnum <- function(nr) {
+      sample(x=c(0.1, 0.2, 0.3),size=nr,replace=T)
+    }
+    c.1.97 <- sapply(rep(nr,97),cnum) # create columns 1-97
+    c.98 <- sample(x=c(-1,0,1),size=nr,replace=T) # create column 98
+    c.99 <- sample(x=letters, size=nr, replace=T) # create column 99
+    # function to generate random string
+    mstring <- function(max=8){
+      pool=c(letters,seq(from=-9, to=9))
+      paste(sample(x=pool, size=sample(1:max,1)),collapse="")
+    }
+    c.100 <- sapply(rep(8,nr),mstring) # create column 100
+    write.table(list(c.1.97,c.98,c.99,c.100), 
+                file = filename, sep = ",", 
+                col.names = FALSE, row.names=FALSE,append=TRUE)
+  }
+  replicate(times,gensize(size,filename))
 }
-c.1.97 <- sapply(rep(nr,97),cnum) # create columns 1-97
-c.98 <- sample(x=c(-1,0,1),size=nr,replace=T) # create column 98
-c.99 <- sample(x=letters, size=nr, replace=T) # create column 99
-# function to generate random string
-mstring <- function(max=8){
-  pool=c(letters,seq(from=-9, to=9))
-  paste(sample(x=pool, size=sample(1:max,1)),collapse="")
-}
-c.100 <- sapply(rep(8,nr),mstring) # create column 100
-file.create("myfile.csv")
-write.table(list(c.1.97,c.98,c.99,c.100), 
-            file = "myfile.csv", sep = ",", 
-            col.names = FALSE, append=TRUE)
+# benchmark
+benchmark(genbig(times=50,size=1000,filename="mytest.csv"),replications=10)
+# Baobao's Computer: Macbook Pro 9.2 (4 GB of RAM), Ubuntu Linux OS
